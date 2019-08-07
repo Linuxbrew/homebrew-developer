b9636156df90348d26342e36994602c861a0c5f8
Issy Long <me@issyl0.co.uk>
2019-08-07 08:56:43 +0100

    Big refactor from review comments

--- cmd/brew-find-formulae-to-bottle.rb
+++ cmd/brew-find-formulae-to-bottle.rb
@@ -6,38 +6,26 @@ module Homebrew
   end
 
   def head_is_merge_commit?
-    `git log --merges -1 --format=%H`.chomp == `git rev-parse HEAD`.chomp
+    Utils.popen_read("git", "log", "--merges", "-1", "--format=%H").chomp == Utils.popen_read("git", "rev-parse", "HEAD").chomp
   end
 
-  def head_has_conflict_lines?
-    @latest_merge_commit_message.include?("Conflicts:") ||
-      @latest_merge_commit_message.include?("Formula/")
+  def head_has_conflict_lines?(commit_message)
+    commit_message.include?("Conflicts:") || commit_message.include?("Formula/")
   end
 
-  def assemble_list_of_formulae_to_bottle
-    @latest_merge_commit_message.each_line do |line|
-      line.strip!
-      next if line.empty? || line == "Conflicts:"
-
-      @formulae_to_bottle.push(line.split('/')[1].split('.')[0])
-    end
-  end
+  formulae_to_bottle = []
+  latest_merge_commit_message = Utils.popen_read("git", "log", "--format=%b", "-1").chomp
 
   odie "You need to be on the master branch to run this." unless on_master?
+  odie "HEAD is not a merge commit." unless head_is_merge_commit?
+  odie "HEAD does not have any bottles to build for new versions." unless head_has_conflict_lines?(latest_merge_commit_message)
 
-  @formulae_to_bottle = []
-  @latest_merge_commit_message = `git log --format=%b -1`.chomp
+  latest_merge_commit_message.each_line do |line|
+    line.strip!
+    next if line.empty? || line == "Conflicts:"
 
-  if head_is_merge_commit?
-    if head_has_conflict_lines?
-      assemble_list_of_formulae_to_bottle
-      puts @formulae_to_bottle
-    else
-      puts "HEAD does not have any bottles to build for new versions, aborting."
-      exit 1
-    end
-  else
-    puts "HEAD is not a merge commit, aborting."
-    exit 1
+    formulae_to_bottle.push(line.split('/')[1].split('.')[0])
   end
+
+  puts @formulae_to_bottle
 end
